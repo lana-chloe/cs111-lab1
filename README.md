d25eb5205eb3dffd332d8441d33f3204221065a3
lana-chloe <lanachloe@g.ucla.edu>
2023-02-03 10:58:58 -0800

    Finished lab1

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
-## UID: 123456789
+## UID: 105817312
 (IMPORTANT: Only replace the above numbers with your true UID, do not modify spacing and newlines, otherwise your tarfile might not be created correctly)
 
 ## Pipe Up
 
-One sentence description
+C implementation of the pipe (|) operator in shells.
 
 ## Building
 
-Explain briefly how to build your program
+Run the following commands to build and run the pipe executable:
+
+    make
+    ./pipe argv[1], argv[2], ... // argv[i] is a command argument
 
 ## Running
 
-Show an example run of your program, using at least two additional arguments, and what to expect
+Show an example run of your program, using at least two additional arguments, and what to expect.
+
+Your output should be the same as if you were to use | between each program in your shell.
+
+    > ls
+    Makefile pipe pipe.c pipe.o README.md
+    > ls | cat | wc
+        5   5   38
+    > ./pipe ls
+    Makefile pipe pipe.c pipe.o README.md
+    > ./pipe ls cat wc
+        5   5   38
 
 ## Cleaning up
 
-Explain briefly how to clean up all binary files
+Run the following command in the terminal:
+    make clean
+
+This cleans up all the binary files existing in the current directory of pipe.c and you can now build pipe again.
--- pipe.c
+++ pipe.c
@@ -1,4 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
 int main(int argc, char *argv[])
 {
-	return 0;
+	dup2(1, 2);
+	if (argc <= 1) {
+		return EINVAL;
+	}
+	else {
+		int fds[2];
+		int prev_fd = STDIN_FILENO;
+		int i;
+		for(i=1;i<=argc-1;i++) {
+			dup2(fds[0], prev_fd);
+			pipe(fds);
+			int return_code = fork();
+			// child process
+			if (return_code == 0) {
+				if (i != argc-1) {
+					dup2(0, fds[0]);
+					dup2(fds[1], 1);
+             		execlp(argv[i], argv[i], NULL);
+                	return errno;
+				}
+				else {
+					dup2(0, fds[0]);
+					execlp(argv[i], argv[i], NULL);
+                	return errno;
+				}
+			}
+			// parent process
+			else if (return_code > 0) {
+				int status;
+				int wait_result = waitpid(return_code, &status, 0);	
+				if (wait_result == -1) { // waitpid unsuccessful
+					perror("waitpid");
+					return errno;
+				}
+				if (WIFEXITED(status)) { // child exited successfully
+					//printf("child exited with status of %d\n", WEXITSTATUS(status));
+					if (WEXITSTATUS(status) != 0) 
+						return WEXITSTATUS(status);
+				}
+      			else return ECHILD; // child killed by signal
+				dup2(prev_fd, fds[1]);
+			}
+			// could not create child process
+			else if (return_code < 0) {
+				perror("fork");
+				return errno;
+			}
+		}
+	}
+	return(0);
 }
